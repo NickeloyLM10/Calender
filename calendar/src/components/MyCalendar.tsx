28e1fd83241adf1c11d9b4801e88590065a9f329
NickeloyLM10 <rocky7739307953@gmail.com>
2025-06-05 02:53:27 +0530

    improved functions

--- calendar/src/components/MyCalendar.tsx
+++ calendar/src/components/MyCalendar.tsx
@@ -93,7 +93,9 @@ function CalendarApp() {
             start,
             end,
             allDay: true,
-            backgroundColor: count > 1 ? '#006400' : '#90ee90',
+            backgroundColor: '', // not needed now
+            display: 'background', // KEY
+            className: count > 1 ? 'dark-green-bg' : 'light-green-bg' // Custom classes
           }
         })
 
@@ -135,29 +137,37 @@ function CalendarApp() {
 
 export default CalendarApp
 
+// Returns ISO week number (1-53) for a given date
 function getISOWeekNumber(date: Date): number {
-  const temp = new Date(date.getTime())
-  temp.setHours(0, 0, 0, 0)
-  temp.setDate(temp.getDate() + 3 - ((temp.getDay() + 6) % 7))
-  const week1 = new Date(temp.getFullYear(), 0, 4)
-  return (
-    1 +
-    Math.round(
-      ((temp.getTime() - week1.getTime()) / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7
-    )
-  )
+  // Copy date to avoid modifying original
+  const temp = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
+  // Set to nearest Thursday (ISO week starts on Monday, Thursday is midweek)
+  temp.setUTCDate(temp.getUTCDate() + 4 - (temp.getUTCDay() || 7))
+  // January 1st of this year
+  const yearStart = new Date(Date.UTC(temp.getUTCFullYear(), 0, 1))
+  // Calculate full weeks to nearest Thursday
+  const weekNo = Math.floor(((temp.getTime() - yearStart.getTime()) / 86400000 - 3 + (yearStart.getUTCDay() || 7)) / 7) + 1
+  return weekNo
 }
 
+// Returns start and end dates (YYYY-MM-DD) of ISO week for a given year and week number
 function getWeekStartEnd(year: number, week: number) {
-  const start = new Date(year, 0, 4)
-  const day = start.getDay() || 7
-  start.setDate(start.getDate() - day + 1 + (week - 1) * 7)
-
-  const end = new Date(start)
-  end.setDate(start.getDate() + 6)
-
+  // January 4th is always in week 1
+  const jan4 = new Date(Date.UTC(year, 0, 4))
+  // Get day of week for Jan 4th (Monday=1,...Sunday=7)
+  const jan4Day = jan4.getUTCDay() || 7
+  // Calculate start of week 1 (Monday)
+  const week1Start = new Date(jan4)
+  week1Start.setUTCDate(jan4.getUTCDate() - jan4Day + 1)
+  // Calculate start of target week
+  const weekStart = new Date(week1Start)
+  weekStart.setUTCDate(week1Start.getUTCDate() + (week - 1) * 7)
+  // Calculate end of week (Sunday)
+  const weekEnd = new Date(weekStart)
+  weekEnd.setUTCDate(weekStart.getUTCDate() + 6)
+  
   return {
-    start: start.toISOString().split('T')[0],
-    end: end.toISOString().split('T')[0],
+    start: weekStart.toISOString().split('T')[0],
+    end: weekEnd.toISOString().split('T')[0],
   }
 }
